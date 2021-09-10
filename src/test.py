84aaf5c2bf84b62ac79a1eae8c292b21bef4ee33
ju754712 <ju754712@rwth-aachen.de>
2021-09-10 13:48:40 +0200

    test

--- src/test.py
+++ src/test.py
@@ -4,10 +4,10 @@ import time
 
 from conv1D_layer import convolution, convolution_cuda
 
-input_length = 10000
-input_depth = 16
+input_length = 1000
+input_depth = 4
 kernel = 3
-layer_depth = 32
+layer_depth = 8
 strides = 1
 dilation = 1
 padding = 0
