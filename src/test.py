c9acea8026bd621e017c60adc12afe3818e922e8
ju754712 <ju754712@rwth-aachen.de>
2021-09-10 16:11:39 +0200

    test

--- src/test.py
+++ src/test.py
@@ -19,14 +19,14 @@ input = np.random.rand(input_length,input_depth)
 weights = np.random.rand(kernel, input_depth, layer_depth)
 bias = np.random.rand(layer_depth)
 
-output2 = convolution_cuda(input, weights, bias,  kernel, layer_depth, strides, dilation, z_padding, padding, a, threads_per_block = 32)
+output2 = convolution_cuda(input, weights, bias,  kernel, layer_depth, strides, dilation, z_padding, padding, a, threads_per_block = 1)
 output2 = convolution(input, weights, bias,  kernel, layer_depth, strides, dilation, z_padding, padding, a)
 
 
 time1 = time.time()
 output2 = convolution(input, weights, bias,  kernel, layer_depth, strides, dilation, z_padding, padding, a)
 time2 = time.time()
-output2 = convolution_cuda(input, weights, bias,  kernel, layer_depth, strides, dilation, z_padding, padding, a, threads_per_block = 64)
+output2 = convolution_cuda(input, weights, bias,  kernel, layer_depth, strides, dilation, z_padding, padding, a, threads_per_block = 1)
 time3 = time.time()
 
 print(time2-time1)
