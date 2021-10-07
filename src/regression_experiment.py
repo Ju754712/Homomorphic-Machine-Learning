b591218837e2225049af8880732eef7c2e3e206d
ju754712 <ju754712@rwth-aachen.de>
2021-10-07 13:04:22 +0200

    added time measurement

--- src/regression_experiment.py
+++ src/regression_experiment.py
@@ -1,5 +1,6 @@
 import numpy as np
 from progress.bar import Bar
+import time
 
 from data_setup import heart_disease_data
 from fc_layer import FCLayer
@@ -17,7 +18,7 @@ input_size, output_size = x_train.shape[2], y_train.shape[2]
 import csv
 
 with open('lin_reg.csv', 'w', newline='') as csvfile:
-    fieldnames = ['PlainOutput', 'BFVOutput', 'CKKSOutput', 'MOREOutput', 'BFVOutput_Enc', 'CKKSOutput_Enc', 'MOREOutput_Enc']
+    fieldnames = ['PlainOutput', 'BFVOutput', 'CKKSOutput', 'MOREOutput', 'BFVOutput_Enc', 'CKKSOutput_Enc', 'MOREOutput_Enc', 'Plain_Time', 'BFV_Time', 'CKKS_Time', 'MORE_Time', 'BFV_Enc_Time', 'CKKS_Enc_Time', 'MORE_Enc_Time']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     writer.writeheader()
@@ -62,8 +63,11 @@ with open('lin_reg.csv', 'w', newline='') as csvfile:
 
             #Encrypt/scale layers params for different schemes
             fc.scale_params_bfv(param_scale)
+
             fc.encrypt_params_ckks(context_ckks)
+
             fc.encrypt_params_more(more)    
+
 
             # Encrypt/scale input
             input_scaled = (x_train[j]*input_scale).astype(int)
@@ -85,16 +89,35 @@ with open('lin_reg.csv', 'w', newline='') as csvfile:
                 for m in range(x_train[j].shape[1]):
                     input_more[k,m] = more.encrypt(x_train[j,k,m])
 
+
+            time1 = time.time()
             output = fc.forward_propagation(x_train[j])
+            time2 = time.time()
             output_bfv = fc.forward_propagation_bfv(input_bfv)
+            time3 = time.time()
             output_ckks = fc.forward_propagation_ckks(input_ckks)
+            time4 = time.time()
             output_more = fc.forward_propagation_more(input_more)
+            time5 = time.time()
+
+            plain_time = time2-time1
+            bfv_time = time3-time2
+            ckks_time = time4-time3
+            more_time = time5-time4
 
             fc.encrypt_params_bfv(context_bfv)
 
+            time1 = time.time()
             output_bfv_enc = fc.forward_propagation_bfv_encrypted(input_bfv_enc)
+            time2 = time.time()
             output_ckks_enc = fc.forward_propagation_ckks_encrypted(input_ckks_enc)
+            time3 = time.time()
             output_more_enc = fc.forward_propagation_more_encrypted(input_more)
+            time4 = time.time()
+            bfv_enc_time = time2-time1
+            ckks_enc_time = time3-time2
+            more_enc_time = time4-time3
+
 
             # print("Output: ", output[0,0])
             # print("Output BFV: ", output_bfv[0,0].decrypt()[0]/scale)
@@ -115,7 +138,7 @@ with open('lin_reg.csv', 'w', newline='') as csvfile:
             #     for j in range(output_more_enc.shape[1]):
             #         print(more.decrypt(output_more_enc[i,j]))
 
-            writer.writerow({'PlainOutput': output[0,0], 'BFVOutput': output_bfv[0,0].decrypt()[0]/scale, 'CKKSOutput': output_ckks.decrypt()[0], 'MOREOutput': more.decrypt(output_more[0,0]), 'BFVOutput_Enc': output_bfv_enc[0,0].decrypt()[0]/scale, 'CKKSOutput_Enc': output_ckks_enc[0,0].decrypt()[0], 'MOREOutput_Enc': more.decrypt(output_more_enc[0,0])})
+            writer.writerow({'PlainOutput': output[0,0], 'BFVOutput': output_bfv[0,0].decrypt()[0]/scale, 'CKKSOutput': output_ckks.decrypt()[0], 'MOREOutput': more.decrypt(output_more[0,0]), 'BFVOutput_Enc': output_bfv_enc[0,0].decrypt()[0]/scale, 'CKKSOutput_Enc': output_ckks_enc[0,0].decrypt()[0], 'MOREOutput_Enc': more.decrypt(output_more_enc[0,0]), 'Plain_Time': plain_time, 'BFV_Time': bfv_time, 'CKKS_Time': ckks_time, 'MORE_Time': more_time, 'BFV_Enc_Time': bfv_enc_time, 'CKKS_Enc_Time': ckks_enc_time, 'MORE_Enc_Time': more_enc_time})
 
             bar.next()
         bar.finish()
