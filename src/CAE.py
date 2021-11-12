915137e9a51ec7fea538c72b2f9ba61efba1359f
ju754712 <ju754712@rwth-aachen.de>
2021-11-12 15:09:17 +0100

    test

--- src/CAE.py
+++ src/CAE.py
@@ -50,7 +50,7 @@ def get_model(inputshape, lr=0.0001):
 
 if __name__ == "__main__":
 
-    PATH = "../CAE/Arrays_AE/Finova2_combined_butter.npy"
+    PATH = "./src/data/train.npy"
     ERROR_SAVE_NAME = "../CAE/sr"
     EXP = 'Finova2'
     EPOCHS2TRAIN = 50
--- src/test.py
+++ src/test.py
@@ -1,23 +1,22 @@
 import numpy as np
-PATH = "./src/data/train.npy"
-# ERROR_SAVE_NAME = "../CAE/sr"
-# EXP = 'Finova2'
-# EPOCHS2TRAIN = 50
-# BATCHSIZE = 4
-# FEATURE = "combined"
-# TRAINON = 100 # or 'all'
-# MODE = 'splits'# 'splits' or 'trainon'
-# SAVE = False
+PATH2 = "./src/data/train.npy"
+PATH = "../CAE/Arrays_AE/Finova2_combined_butter.npy"
+EXP = 'Finova2'
+EPOCHS2TRAIN = 50
+BATCHSIZE = 4
+FEATURE = "combined"
+TRAINON = 100 # or 'all'
+MODE = 'splits'# 'splits' or 'trainon'
+SAVE = False
 
-# data = np.load(PATH, mmap_mode='r') # load data
+data = np.load(PATH, mmap_mode='r+') # load data
 
 
-# data = data[4000]
-# with open('./src/data/train.npy', 'wb') as f:
+data = data[0:4000]
+with open(PATH2, 'wb') as f:
+    np.save(f, data)
 
-#     np.save(f, data)
-
-data = np.load(PATH, mmap_mode='r')
+data = np.load(PATH2, mmap_mode='r')
 
 print(data.shape)
 
