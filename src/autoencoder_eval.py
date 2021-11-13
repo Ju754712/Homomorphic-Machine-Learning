955a1120c3610957be3439177d1d43baa35fa8f7
ju754712 <ju754712@rwth-aachen.de>
2021-11-13 23:54:31 +0100

    test

--- src/autoencoder_eval.py
+++ src/autoencoder_eval.py
@@ -66,7 +66,7 @@ autodecoder_plain.remove(2)
 
 print(autoencoder_plain.layers)
 print(autodecoder_plain.layers)
-with open('autoencoder_more.csv', 'w', newline='') as csvfile:
+with open('./src/csv/autoencoder_more.csv', 'w', newline='') as csvfile:
     fieldnames = ['encoding_accuracy', 'decoding_accuracy_plain', 'decoding_accuracy_more', 'decoding_accuracy', 'encoder_input_encryption_time', 'encoder_plain_time', 'encoder_more_time', 'encoder_output_decryption_time', 'decoder_input_encryption_time', 'decoder_plain_time', 'decoder_more_time', 'decoder_output_decryption_time' ]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
