dfa99345a29d70f70690b91c092f8e3b13b961f6
ju754712 <ju754712@rwth-aachen.de>
2021-11-20 14:31:10 +0100

    test

--- src/autoencoder_eval_ckks.py
+++ src/autoencoder_eval_ckks.py
@@ -43,60 +43,60 @@ context = ts.context(
 )
 
 # set the scale
-context.global_scale = pow(2, bits_scale)
+# context.global_scale = pow(2, bits_scale)
 
-# galois keys are required to do ciphertext rotations
-context.generate_galois_keys()
+# # galois keys are required to do ciphertext rotations
+# context.generate_galois_keys()
 
-print(dir(ts))
+# print(dir(ts))
 
-arraylength = x_test.shape[1]
+# arraylength = x_test.shape[1]
 
-with open('./src/csv/autoencoder_ckks.csv', 'w', newline='') as csvfile:
-    fieldnames = ['encoding_accuracy', 'decoding_accuracy_plain', 'decoding_accuracy_ckks', 'decoding_accuracy', 'encoder_input_encryption_time', 'encoder_plain_time', 'encoder_more_time', 'encoder_output_decryption_time', 'decoder_input_encryption_time', 'decoder_plain_time', 'decoder_more_time', 'decoder_output_decryption_time' ]
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+# with open('./src/csv/autoencoder_ckks.csv', 'w', newline='') as csvfile:
+#     fieldnames = ['encoding_accuracy', 'decoding_accuracy_plain', 'decoding_accuracy_ckks', 'decoding_accuracy', 'encoder_input_encryption_time', 'encoder_plain_time', 'encoder_more_time', 'encoder_output_decryption_time', 'decoder_input_encryption_time', 'decoder_plain_time', 'decoder_more_time', 'decoder_output_decryption_time' ]
+#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    writer.writeheader()
-    # bar = Bar("Processing...", max = x_test.shape[0])
-    for i in range(x_test.shape[0]):
-        # bar.next()
-        time_d = time.time()
+#     writer.writeheader()
+#     # bar = Bar("Processing...", max = x_test.shape[0])
+#     for i in range(x_test.shape[0]):
+#         # bar.next()
+#         time_d = time.time()
         
-        print("Encryption")
-        x_test_ckks = np.zeros((1,x_test.shape[1], x_test.shape[2]), dtype=object)
-        time1 = time.time()
-        for k in range(x_test[i].shape[0]):
-            for j in range(x_test[i].shape[1]):
-                x_test_ckks[0,k,j] = ts.CKKSVector(context, [x_test[i,k,j]])
-        time2 = time.time()
-        encoder_input_encryption_time = time2-time1
-        print("Encoder Input Encryption Time: ", encoder_input_encryption_time)
+#         print("Encryption")
+#         x_test_ckks = np.zeros((1,x_test.shape[1], x_test.shape[2]), dtype=object)
+#         time1 = time.time()
+#         for k in range(x_test[i].shape[0]):
+#             for j in range(x_test[i].shape[1]):
+#                 x_test_ckks[0,k,j] = ts.CKKSVector(context, [x_test[i,k,j]])
+#         time2 = time.time()
+#         encoder_input_encryption_time = time2-time1
+#         print("Encoder Input Encryption Time: ", encoder_input_encryption_time)
 
-        time1 = time.time()
-        encoding_plain = autoencoder_plain.predict(x_test[i,:,:].reshape((1,arraylength,1)))
-        time2 = time.time()
-        encoding_ckks_enc = autoencoder_ckks.predict_ckks(x_test_ckks)
-        time3 = time.time()
+#         time1 = time.time()
+#         encoding_plain = autoencoder_plain.predict(x_test[i,:,:].reshape((1,arraylength,1)))
+#         time2 = time.time()
+#         encoding_ckks_enc = autoencoder_ckks.predict_ckks(x_test_ckks)
+#         time3 = time.time()
 
-        encoder_plain_time = time2-time1
-        encoder_ckks_time = time3-time2
-        print("Encoder Plain Time: ", encoder_plain_time)
-        print("Encoder CKKS TIme: ", encoder_plain_time)
+#         encoder_plain_time = time2-time1
+#         encoder_ckks_time = time3-time2
+#         print("Encoder Plain Time: ", encoder_plain_time)
+#         print("Encoder CKKS TIme: ", encoder_plain_time)
 
 
 
-        time1 = time.time()
-        encoding_more = np.zeros((1,encoding_ckks_enc[0].shape[0], encoding_ckks_enc[0].shape[1]))
-        for k in range(encoding_ckks_enc[0].shape[0]):
-            for j in range(encoding_ckks_enc[0].shape[1]):
-                encoding_more[0,k,j] = encoding_ckks_enc[0][k,j]
+#         time1 = time.time()
+#         encoding_more = np.zeros((1,encoding_ckks_enc[0].shape[0], encoding_ckks_enc[0].shape[1]))
+#         for k in range(encoding_ckks_enc[0].shape[0]):
+#             for j in range(encoding_ckks_enc[0].shape[1]):
+#                 encoding_more[0,k,j] = encoding_ckks_enc[0][k,j]
 
-        time2 = time.time()
+#         time2 = time.time()
 
-        encoder_output_decryption_time = time2-time1
-        encoding_accuracy = mse(encoding_plain[0], encoding_more[0])
-        print("Encoder Output Decryption Time: ", encoder_output_decryption_time)
-        print(encoding_accuracy)
+#         encoder_output_decryption_time = time2-time1
+#         encoding_accuracy = mse(encoding_plain[0], encoding_more[0])
+#         print("Encoder Output Decryption Time: ", encoder_output_decryption_time)
+#         print(encoding_accuracy)
 
         # time1 = time.time()
         # encoding_more_enc = np.zeros((encoding_more.shape[0],encoding_more.shape[1], encoding_more.shape[2],2,2))
