b1711a2da8df2416f22553e9911805ba7e699a9c
ju754712 <ju754712@rwth-aachen.de>
2021-11-20 16:53:11 +0100

    test

--- src/test.py
+++ src/test.py
@@ -1,11 +1,12 @@
 import tenseal as ts
+import numpy as np
 
 bits_scale = 26
 
 # Create TenSEAL context
 context = ts.context(
     ts.SCHEME_TYPE.CKKS,
-    poly_modulus_degree=8192,
+    poly_modulus_degree= 4*16384, #8192,
     coeff_mod_bit_sizes=[31, bits_scale, bits_scale, bits_scale, bits_scale, bits_scale, bits_scale, 31]    
 )
 
@@ -15,6 +16,22 @@ context.global_scale = pow(2, bits_scale)
 # galois keys are required to do ciphertext rotations
 context.generate_galois_keys()
 
-x_enc, windows_nb = ts.im2col_encoding(context, [[1],[2],[3],[4],[5],[6],[7]], 3, 1, 1)
-weight = [1,2,3]
+
+PATH = "./src/data/train.npy"
+data = np.load(PATH, mmap_mode='r')
+x_test = data[0]
+print(x_test.shape)
+x_test = np.transpose(x_test,(1,0))
+print(x_test)
+
+
+
+x_enc, windows_nb = ts.im2col_encoding(context, x_test, 1, 1, 1)
+print(len(x_enc.decrypt()))
+# print(windows_nb)
+# kernel = [[1,2]]
+
+# o_dec = x_enc.conv2d_im2col(kernel, windows_nb) 
+# o = o_dec.decrypt()
+# print(o)
 
