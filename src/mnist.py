4efa30b4b34c62c466d54d9d9ccd9791d56e77dd
ju754712 <ju754712@rwth-aachen.de>
2021-11-25 21:52:57 +0100

    te

--- src/mnist.py
+++ src/mnist.py
@@ -41,8 +41,8 @@ y_test = np_utils.to_categorical(y_test)
 # x_train = x_train[0:4000]
 # y_train = y_train[0:1000]
 
-x_test = x_test[0:100]
-y_test = y_test[0:100]
+x_test = x_test[0:10000]
+y_test = y_test[0:10000]
 
 more = MoreScheme(200)
 time1 = time.time()
