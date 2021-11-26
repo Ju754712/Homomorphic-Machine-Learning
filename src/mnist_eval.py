021f224c8d75dcbf4ac77e6bb416c6d814e984d2
ju754712 <ju754712@rwth-aachen.de>
2021-11-27 00:43:49 +0100

    test

--- src/mnist_eval.py
+++ src/mnist_eval.py
@@ -176,6 +176,6 @@ print("Correct: ", correct, ", incorrect: ", incorrect)
 # print("Correct: ", correct, ", incorrect: ", incorrect)
 
 for i in range(1000):
-    if np.argmax(output[i]) != np.argmax(output_more[i][0]):
+    if np.argmax(output[i][0]) != np.argmax(output_more[i][0]):
         print(output_more[i])
         print(output[i])
