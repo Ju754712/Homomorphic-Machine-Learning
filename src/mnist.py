699ea7f3a323672b6fc0eba99045e81191671f13
ju754712 <ju754712@rwth-aachen.de>
2021-11-26 19:17:34 +0100

    test

--- src/mnist.py
+++ src/mnist.py
@@ -15,7 +15,7 @@ import random
 import random
 from math import floor
 
-EPOCHS = 1
+EPOCHS = 35
 BATCH_SIZE = 1
 LEARNING_RATE = 0.1
 
@@ -49,23 +49,23 @@ y_train = y_train[0:60000]
 
 more = MoreScheme(200)
 time1 = time.time()
-print("Encrypting Input")
-x_train_enc = np.zeros((x_train.shape[0],x_train.shape[1], x_train.shape[2],2,2))
-for i in range(x_train.shape[0]):
-    for j in range(x_train.shape[1]):
-        for k in range(x_train.shape[2]):
-            x_train_enc[i,j,k] = more.encrypt(x_train[i,j,k])
-time2 = time.time()
-print("Encrypting Output")
-y_train_enc = np.zeros((y_train.shape[0],y_train.shape[1],2,2))
-for i in range(y_train.shape[0]):
-    for j in range(y_train.shape[1]):
-        y_train_enc[i,j] = more.encrypt(y_train[i,j])
-time3 = time.time()
+# print("Encrypting Input")
+# x_train_enc = np.zeros((x_train.shape[0],x_train.shape[1], x_train.shape[2],2,2))
+# for i in range(x_train.shape[0]):
+#     for j in range(x_train.shape[1]):
+#         for k in range(x_train.shape[2]):
+#             x_train_enc[i,j,k] = more.encrypt(x_train[i,j,k])
+# time2 = time.time()
+# print("Encrypting Output")
+# y_train_enc = np.zeros((y_train.shape[0],y_train.shape[1],2,2))
+# for i in range(y_train.shape[0]):
+#     for j in range(y_train.shape[1]):
+#         y_train_enc[i,j] = more.encrypt(y_train[i,j])
+# time3 = time.time()
 
-print("Time for x_train encryption: ", time2-time1)
+# print("Time for x_train encryption: ", time2-time1)
 
-print("Time for y_train encryption: ", time3-time2)
+# print("Time for y_train encryption: ", time3-time2)
 
 net_sigmoid = Network()
 net_sigmoid.add(FCLayer(28*28, 100))                # input_shape=(1, 28*28)    ;   output_shape=(1, 100)
@@ -91,17 +91,17 @@ net_sigmoid_more.use(mse, mse_prime_more)
 time1 = time.time()
 net_sigmoid.fit(x_train, y_train, epochs=EPOCHS, learning_rate = LEARNING_RATE, batch_size =BATCH_SIZE)
 time2 = time.time()
-net_sigmoid_more.fit_more(x_train_enc, y_train_enc, epochs=EPOCHS, learning_rate = LEARNING_RATE, batch_size =BATCH_SIZE, more=more)
-time3 = time.time()
+# net_sigmoid_more.fit_more(x_train_enc, y_train_enc, epochs=EPOCHS, learning_rate = LEARNING_RATE, batch_size =BATCH_SIZE, more=more)
+# time3 = time.time()
 
 print("Training Sigmoid Plain: ",time2-time1)
-print("Training Sigmoid More: ",time3-time2)
+# print("Training Sigmoid More: ",time3-time2)
 
 # for i in range(3):
 #     net_sigmoid_more.layers[2*i].decrypt_params_more(more)
 
 net_sigmoid.save("src/params/mnist_sigmoid")
-net_sigmoid_more.save("src/params/mnist_sigmoid_more")
+# net_sigmoid_more.save("src/params/mnist_sigmoid_more")
 
 net_sigmoid = Network()
 net_sigmoid.add(FCLayer(28*28, 100))                # input_shape=(1, 28*28)    ;   output_shape=(1, 100)
